f47eccf72eaa0f026b828cf0252d2c2b6fb48649
wade.li <wadelicw@gmail.com>
2024-02-07 15:05:46 +0800

    add findByTitleContaining api

--- backend/src/main/java/com/wade/spring/demo/backend/dao/BookRepository.java
+++ backend/src/main/java/com/wade/spring/demo/backend/dao/BookRepository.java
@@ -1,9 +1,12 @@
 package com.wade.spring.demo.backend.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wade.spring.demo.backend.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
