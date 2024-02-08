be74d2e4a9f035bbc8b069c3d1ec925c733c8338
wade.li <wadelicw@gmail.com>
2024-02-08 17:24:45 +0800

    remove console log

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -12,12 +12,7 @@ const handler = NextAuth({
   },
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: {},
         password: {}
@@ -30,7 +25,6 @@ const handler = NextAuth({
         });
         if (user) {
           const passwordCompare = await compare(credentials?.password || "", user.password);
-          console.log("is correct pw: ", passwordCompare);
           if (passwordCompare) {
             return {
               id: user.id,
--- frontend/src/app/api/auth/register/route.ts
+++ frontend/src/app/api/auth/register/route.ts
@@ -5,15 +5,13 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
-    //validate email and password
     const hashedPassword = await hash(password, 10);
-    const result = await prisma.user.create({
+    await prisma.user.create({
       data: {
         email,
         password: hashedPassword
       }
     });
-    console.log(result);
   } catch (error) {
     console.log({ error });
   }
